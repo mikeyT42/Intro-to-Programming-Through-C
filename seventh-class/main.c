b09fb27b58ab7e3c34e322bb4e73a4640857ddf3
mikeyT42 <mrmojorisen0@gmail.com>
2024-08-13 23:11:33 -0400

    Getting the seventh class ready. This one is finally on dynamic memory and will be the final class in the course.

---
+++ seventh-class/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void understanding_malloc(void);
+void undertanding_calloc(void);
+void understanding_realloc(void);
+
+// -----------------------------------------------------------------------------
+int main(void) {
+  system("clear");
+  puts("---------------------------------------------------------------------");
+  puts("\t\tThis is the \"Whiteboard\" for today.");
+  puts("---------------------------------------------------------------------"
+       "\n");
+
+  return EXIT_SUCCESS;
+}
