e683bd64f89fc489128ce6551527097d70702f85
mikeyT42 <mrmojorisen0@gmail.com>
2024-08-13 23:23:31 -0400

    Added some more function declarations.

--- seventh-class/main.c
+++ seventh-class/main.c
@@ -4,6 +4,12 @@
 void understanding_malloc(void);
 void undertanding_calloc(void);
 void understanding_realloc(void);
+void understanding_free(void);
+
+int *returning_a_local_array(void);
+char *returning_a_built_string(void);
+
+void security_vulnerability_with_realloc(void);
 
 // -----------------------------------------------------------------------------
 int main(void) {
