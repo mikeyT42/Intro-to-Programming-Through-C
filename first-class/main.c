8b0661aec194b32077614b8dc27094c61578fd94
mikeyT42 <mrmojorisen0@gmail.com>
2024-08-31 23:43:58 -0400

    Defining sections.

---
+++ first-class/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void what_is_a_program(void);
+void how_does_a_program_execute(void);
+
+void general_syntax(void);
+void what_is_syntax(void);
+void declaring_and_defining_variables(void);
+void declaring_defining_and_calling_functions(void);
+int function_a(void);
+int function_b(int x);
+void comments(void);
+
+void intro_to_arrays(void);
+void enums(void);
+void structs(void);
+
+// -----------------------------------------------------------------------------
+int main(void) {
+  system("clear");
+  puts("---------------------------------------------------------------------");
+  puts("\t\tThis is the \"Whiteboard\" for today.");
+  puts("---------------------------------------------------------------------"
+       "\n");
+
+  return EXIT_SUCCESS;
+}
