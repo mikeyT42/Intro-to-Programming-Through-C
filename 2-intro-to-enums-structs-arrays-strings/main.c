054e77032ad9cb250e40186fc96d0474bd71381e
mikeyT42 <mrmojorisen0@gmail.com>
2024-09-19 00:55:19 -0400

    Almost done with structs. It is getting too late and I need to go to bed. I want to add an example of a struct that doesn't pad.

--- 2-intro-to-enums-structs-arrays-strings/main.c
+++ 2-intro-to-enums-structs-arrays-strings/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/_types/_size_t.h>
 
 #define NAME_LEN 30
 
@@ -12,13 +13,13 @@ void strings(void);
 void string_declarations(void);
 void string_to_functions(char string[]);
 
-typedef enum { RED, GREEN, BLUE } Color;
+typedef enum { RED, GREEN, BLUE } color;
 void enums(void);
 
 typedef struct {
   int age;
   char name[NAME_LEN];
-} Person;
+} person;
 void structs(void);
 
 // -----------------------------------------------------------------------------
@@ -253,7 +254,7 @@ void enums() {
        "part the enum to a variable of our enum type. Let's see this in\n"
        "practice.\n");
 
-  Color red = RED;
+  color red = RED;
 
   puts("Look at that! Pretty neat right! We can make grouped symbols with\n"
        "symbolic values. Now, why would we do this? For one, we can make\n"
@@ -267,7 +268,7 @@ void enums() {
        "something. Let's output our enumeration.\n");
 
   printf("red = %i\n", red);
-  Color green = GREEN;
+  color green = GREEN;
   printf("green = %i\n\n", green);
 
   puts("Wow, look at that, 2 integers that have gone up incrementally.\n"
@@ -278,16 +279,102 @@ void enums() {
 
 // -----------------------------------------------------------------------------
 void structs() {
+  puts("----------------------------------------------------");
+  puts("\tIntroduction To Structures.");
+  puts("----------------------------------------------------");
 
-  Person michael = {.age = 28, .name = "Michael"};
+  puts("Structures allow us to define complex types in C. This allows us to\n"
+       "define data that should be grouped together to represent something in\n"
+       "our code. Structures are an incredibly powerful tool for the\n"
+       "developer to utilize. Let's go over structures with an example. I\n"
+       "have defined a structure at the top of this file. First, we use the\n"
+       "typedef keyword; after that the struct keyword is used to signify a\n"
+       "struct definition is coming; then the curly brackets are used to hold\n"
+       "the declarations of the data members that are a part of this struct.\n"
+       "These data members are given a type and a name: each member goes on\n"
+       "its own line and is ended with a semicolon. After the curly brackets\n"
+       "goes the name of the struct itself. Now, how do we use our defined\n"
+       "struct? Let's see the syntax below.\n");
 
-  Person thomas;
+  person michael = {.age = 28, .name = "Michael"};
+  printf("name = %s, age = %i\n\n", michael.name, michael.age);
+
+  puts("I have declared a variable of type person with a variable name of\n"
+       "michael. I have then initialized the variable at the same time: the\n"
+       "syntax to do this is with the curly brackets. Each data member in the\n"
+       "defined struct must be given a value, so we give it one. We do this\n"
+       "by using the \"dot operator\" before the name of the data member and\n"
+       "then assign a value to that data member: each data member is\n"
+       "separated with a comma. We could optionally not include the data\n"
+       "member names in the declaration, but the value need to exactly match\n"
+       "the struct definition order. If there are 2 integers defined next to\n"
+       "each other, it gets confusing. I think it is a bad practice, so I\n"
+       "will enforce the way I have shown above. Now, how do we use the data\n"
+       "in the struct? We use the variable name, and then access each member\n"
+       "using the dot operator like how we've done in the call to printf().\n"
+       "How, though, do we assign values to a struct's members after we have\n"
+       "already declared it. This can be problematic depending on the kinds\n"
+       "of data in the struct. Let's try this with our struct.\n");
+
+  person thomas;
   thomas.age = 14;
+  // This line creates an error.
   // thomas.name = "Thomas";
   strcpy(thomas.name, "Thomas");
+  printf("name = %s, age = %i\n\n", thomas.name, thomas.age);
 
-  printf("name = %s, age = %i\n", michael.name, michael.age);
-  printf("name = %s, age = %i\n", thomas.name, thomas.age);
+  puts("Here we see that we can assign to the age member just fine after\n"
+       "declaration, but we can't simply assign to the name member\n"
+       "afterwords: why is that? Strings--and arrays in general--are special.\n"
+       "Arrays can't be assigned to in bulk after declaration like primitive\n"
+       "types. This is because after declaration they are initialized to\n"
+       "something; either what we say or what C says. If we don't say, then C\n"
+       "initializes \"zero values\" to the members of the array: for integers\n"
+       "that is 0, for floating point (decimal) numbers it is 0.0, and for\n"
+       "strings it is a bunch of null-terminator characters. So, to change\n"
+       "those characters later we have to assign to the elements in that\n"
+       "string one at a time. We can do this ourselves, or use functions\n"
+       "provided to us in the standard library. In the above example I opt to\n"
+       "call strcpy() from the string.h library because we haven't gone over\n"
+       "loops. We provide a string to this function and now we have\n"
+       "\"assigned\" to our name string.\n");
 
-  printf("sizeof Person = %li\n", sizeof(Person));
+  const size_t size_int = sizeof(int);
+  const size_t size_char = sizeof(char);
+  const size_t size_name_len_char = sizeof(char[NAME_LEN]);
+  printf(
+      "We will now go over something that is moderately important about\n"
+      "structs that you should be aware of: how C sizes them. Depending on\n"
+      "the data member types and how many data members there, the size of a\n"
+      "defined struct will change. Let's see if we can figure out the size\n"
+      "of our person struct. Here are the sizes of it's data members:\n\n"
+      "\tsizeof(int)      = %lu\n"
+      "\tsizeof(char)     = %lu\n"
+      "\tsizeof(char[%i]) = %lu\n\n"
+      "Knowing these values, and knowing that we have an int and a char array\n"
+      "with a length of %i as a part of our person struct we can calculate\n"
+      "the size of our person struct to be:\n\n"
+      "\t%lu + %lu = %lu.\n\n"
+      "Let's see if we are correct:\n\n"
+      "\tsizeof(person) = %lu\n\n"
+      "Woa! We aren't correct? The math is so simple, what could possibly be\n"
+      "wrong? Well, something special is happening when it comes to structure\n"
+      "sizing.\n\n",
+      size_int, size_char, NAME_LEN, size_name_len_char, NAME_LEN, size_int,
+      size_name_len_char, size_int + size_name_len_char, sizeof(person));
+
+  puts("The size of a structure is determined not just by the data member\n"
+       "types, but also by padding. Depending on the data member types, C may\n"
+       "insert padding between the data members in memory. What does this\n"
+       "\"look\" like in memory? Here is a way it can be shown.\n");
+  puts("\t|  age  | 0x0\n"
+       "\t|  age  | 0x1\n"
+       "\t|  age  | 0x2\n"
+       "\t|  age  | 0x3\n"
+       "\t|  pad  | 0x4\n"
+       "\t|  pad  | 0x5\n"
+       "\t|  name | 0x6\n"
+       "\t|  name | 0x7\n"
+       "\t|  ...  | ...\n"
+       "\t|  name | 0x36\n\n");
 }
