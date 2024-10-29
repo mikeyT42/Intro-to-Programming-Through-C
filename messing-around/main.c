d7d1405ae019a599e10e301f8e33d17f026995bb
mikeyT42 <mrmojorisen0@gmail.com>
2024-10-28 23:41:19 -0400

    I am saving the emails in order to not have to rewrite them all of the time.

--- messing-around/main.c
+++ messing-around/main.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int keystrokes(char input[]);
+void echo(int my_int);
+int return_my_int(int my_int);
+char i_am_a(void);
+int add(int x, int y);
 
 int main(void) {
-    char in[] = "Hello, World!\n";
-    int key = keystrokes(in);
+  puts("I am in main.");
+  echo(10);
+  puts("I am in main.");
+  echo(2);
+  puts("I am in main.");
+  int my_int_is_ten = 10;
+  my_int_is_ten = return_my_int(my_int_is_ten);
+  printf("i_am_a returned %c.\n", i_am_a());
 
-    printf("keystrokes = %i\n", key);
+  int z = add(9, 10);
+  printf("z = %d\n", z);
+
+  int e = 20;
+  int o = 30;
+  z = add(e, o);
+  printf("z = %d\n", z);
+
+  return EXIT_SUCCESS;
 }
 
-// -----------------------------------------------------------------------------
-int keystrokes(char input[]) {
-    int keystrokes = 0;
-    for (int index = 0; input[index] != '\n'; index++) {
-        keystrokes++;
-    }
+void echo(int my_int) {
+  puts("I am echo.");
+  printf("%d\n", my_int);
+}
 
-    return keystrokes;
+int return_my_int(int my_int) {
+    return my_int;
+}
+
+char i_am_a() {
+   return 'b'; 
+}
+
+int add(int x, int y) {
+   printf("x = %d\n", x);
+   printf("y = %d\n", y);
+   printf("x + y = %d\n", x + y);
+   return x + y;
 }
